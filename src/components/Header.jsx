10cd36b6e5b2460a0c6bd4c5bb9c3276304ad037
Alexandra <alexandra.nadine.lewis@gmail.com>
2025-06-02 19:05:24 -0700

    font changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-moss text-white p-4 mb-4">
-      <h1 className="text-xl font-bold">PLANTY 🌿 </h1>
+      <h1 className="text-5xl font-bold">Planty 🌿 </h1>
     </header>
   );
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,10 +3,10 @@ export default function Modal({ isOpen, onClose, children }) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-      <div className="bg-white p-6 rounded shadow-lg w-full max-w-md relative">
+      <div className="bg-white p-8 rounded shadow-lg w-full max-w-md relative">
         {/* Close Button */}
         <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-800 p-1"
           onClick={onClose}
         >
           ✕
